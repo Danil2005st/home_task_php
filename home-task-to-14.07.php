9216d733d48c4717201e0e94e050ac8fe6593a90
Danil2005st <Danil2005st@gmail.com>
2017-07-14 15:23:30 +0300

    добавлена сортировка

--- home-task-to-14.07.php
+++ home-task-to-14.07.php
@@ -46,35 +46,71 @@
         <p>сортировка случайных чисел массива</p>
         
         <?php
-        echo "<br>";
+        function startSort(array $sortArr){
+	$ret = '';
+	foreach ($sortArr as $oneColumn)
+		$ret[] = bubleSort($oneColumn);
+
+	return $ret;
+}
+
+function bubleSort(array $sortArr){
+
+	if(count($sortArr)) {
+		do {
+			$isSorted = false;
+
+			$baseValue = $sortArr[0];
+			for($i=1;$i<count($sortArr);$i++){
+				if($baseValue>$sortArr[$i]){
+					$sortArr[$i-1] = $sortArr[$i];
+					$isSorted = true;
+				}else{
+					$sortArr[$i-1] = $baseValue;
+					$baseValue = $sortArr[$i];
+				}
+			}
+
+			$sortArr[count($sortArr)-1] = $baseValue;
+		} while ( $isSorted );
+	}
+
+	return $sortArr;
+}
+
+echo "<br>";
         echo "<br>";
         echo "<br>";
             $a = array();
             for ($ascending=0; $ascending<10; $ascending++) {
-                for ($ascending2=0; $ascending2<10; $ascending2++) {
-                    $a[$ascending][$ascending2]=rand(0, 1000);
-                    echo $a[$ascending][$ascending2];
-                }
-                echo "<br>";
+	            for ($ascending2=0; $ascending2<10; $ascending2++) {
+		            $a[$ascending][$ascending2]=rand(0, 1000);
+		            echo $a[$ascending][$ascending2];
+	            }
+	            echo "<br>";
             }
-           
+
         echo "<br>";
         echo "<br>";
         echo "<br>";
-            
-            
-            for($i=count($a); $i>=0; $i--){
-                $a[$i] = $i * 2;
-            }
-            
-            
-            
+
+
+
+
+
         echo "<br>";
         echo "<br>";
         echo "<br>";
             echo "<PRE>";
                 print_r($a);
             echo "</PRE>";
+
+
+            echo 'Результаты сортировки';
+
+echo "<PRE>";
+print_r(startSort($a));
+echo "</PRE>";
         ?>
         
     </body>
